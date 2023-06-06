98255a704d77f3d6df15cca0bd79b947561daa5b
hasundue <hasundue@gmail.com>
2023-06-06 09:42:12 +0900

    test(source): use DenopsStub

--- tests/source_test.ts
+++ tests/source_test.ts
@@ -1,39 +1,41 @@
-import { assertEquals } from "https://deno.land/std@0.182.0/testing/asserts.ts";
-import { stub } from "https://deno.land/std@0.182.0/testing/mock.ts";
-import { test } from "https://deno.land/x/denops_test@v1.1.0/mod.ts";
-import type { Denops } from "https://deno.land/x/ddu_vim@v2.7.0/deps.ts";
+import "https://deno.land/std@0.190.0/dotenv/load.ts";
+import { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
+import {
+  beforeEach,
+  describe,
+  it,
+} from "https://deno.land/std@0.190.0/testing/bdd.ts";
+import { collect } from "https://deno.land/x/streamtools@v0.5.0/collect.ts";
 import { Source } from "../denops/@ddu-sources/template.ts";
+import { DenopsStub } from "https://raw.githubusercontent.com/vim-denops/deno-denops-test/7c6500cc94c861ad1a2e13fbc8fa018081499bc9/denops_test/mod.ts";
 
-function denopsStub(denops: Denops) {
-  stub(denops, "call", async (name: string, ...args: unknown[]) => {
-    if (name === "list") {
-      return ["ddu", "template"];
-    }
-    return await denops.call(name, ...args);
+describe("Source", () => {
+  const denops = new DenopsStub({
+    call: async (name: string, ...args: unknown[]) => {
+      if (name === "list") {
+        return ["ddu", "template"];
+      }
+      return await denops.call(name, ...args);
+    },
   });
-}
 
-test("any", "ensure the connection with denops", (denops) => {
-  assertEquals(denops.name, "@denops-test");
-});
+  let source = new Source();
 
-test("any", "gather", async (denops) => {
-  denopsStub(denops);
-  const source = new Source();
-  const stream = source.gather({
-    denops,
-    sourceParams: source.params(),
+  beforeEach(() => {
+    source = new Source();
   });
-  assertEquals(
-    await readAll(stream),
-    [{ word: "ddu" }, { word: "template" }],
-  );
-});
 
-async function readAll<T>(stream: ReadableStream<T[]>): Promise<T[]> {
-  const items: T[] = [];
-  for await (const chunk of stream) {
-    items.push(...chunk);
-  }
-  return items;
-}
+  describe("gather", () => {
+    const stream = source.gather({
+      denops,
+      sourceParams: source.params(),
+    });
+
+    it("should list all words", async () => {
+      assertEquals(
+        await collect(stream),
+        [[{ word: "ddu" }, { word: "template" }]],
+      );
+    });
+  });
+});
