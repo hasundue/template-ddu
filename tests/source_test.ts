ba4763826f2c97321c2a1cee1d4576d883b16eac
hasundue <hasundue@gmail.com>
2023-06-07 15:56:02 +0900

    test(source): remove dependency to dotenv

--- tests/source_test.ts
+++ tests/source_test.ts
@@ -1,4 +1,3 @@
-import "https://deno.land/std@0.190.0/dotenv/load.ts";
 import { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
 import {
   beforeEach,
@@ -7,7 +6,7 @@ import {
 } from "https://deno.land/std@0.190.0/testing/bdd.ts";
 import { collect } from "https://deno.land/x/streamtools@v0.5.0/collect.ts";
 import { Source } from "../denops/@ddu-sources/template.ts";
-import { DenopsStub } from "https://raw.githubusercontent.com/vim-denops/deno-denops-test/7c6500cc94c861ad1a2e13fbc8fa018081499bc9/denops_test/mod.ts";
+import { DenopsStub } from "https://raw.githubusercontent.com/vim-denops/deno-denops-test/d25ae6a557e46e87666b23cbd96364fd119b3563/denops_test/mod.ts";
 
 describe("Source", () => {
   const denops = new DenopsStub({
