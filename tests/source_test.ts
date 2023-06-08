47ec5986489b3dbc9bf92eb2e998ce314468c816
hasundue <hasundue@gmail.com>
2023-06-09 08:32:14 +0900

    test(source): update denops_test to v1.4.0

--- tests/source_test.ts
+++ tests/source_test.ts
@@ -1,12 +1,8 @@
 import { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
-import {
-  beforeEach,
-  describe,
-  it,
-} from "https://deno.land/std@0.190.0/testing/bdd.ts";
+import { describe, it } from "https://deno.land/std@0.190.0/testing/bdd.ts";
 import { collect } from "https://deno.land/x/streamtools@v0.5.0/collect.ts";
 import { Source } from "../denops/@ddu-sources/template.ts";
-import { DenopsStub } from "https://raw.githubusercontent.com/vim-denops/deno-denops-test/d25ae6a557e46e87666b23cbd96364fd119b3563/denops_test/mod.ts";
+import { DenopsStub } from "https://deno.land/x/denops_test@v1.4.0/mod.ts";
 
 describe("Source", () => {
   const denops = new DenopsStub({
@@ -18,11 +14,7 @@ describe("Source", () => {
     },
   });
 
-  let source = new Source();
-
-  beforeEach(() => {
-    source = new Source();
-  });
+  const source = new Source();
 
   describe("gather", () => {
     const stream = source.gather({
